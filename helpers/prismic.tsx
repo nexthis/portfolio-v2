4fc16f950325a9805db0e1fcc20cd93bcb5aefad
nexthis <pawel.romanowski000@gmail.com>
2021-08-18 18:22:06 +0200

    rm custom link and fix ts types

--- helpers/prismic.tsx
+++ helpers/prismic.tsx
@@ -6,21 +6,13 @@ import {
     linkResolver,
     hrefResolver
 } from 'prismic-configuration'
-import {ReactChild} from "react";
-
-// Helper function to convert Prismic Rich Text links to Next/Link components
-export const customLink = ( element, content: ReactChild) => (
-    <Link key={element.data.id} href={hrefResolver(element.data)} as={linkResolver(element.data)}>
-        <a>{content}</a>
-    </Link>
-)
 
 // Client method to query documents from the Prismic repo
 export const Client = (req = null) => (
     Prismic.client(apiEndpoint, createClientOptions(req, accessToken))
 )
 
-const createClientOptions = (req = null, prismicAccessToken = null) => {
+const createClientOptions = (req = null, prismicAccessToken:string = '') => {
     const reqOption = req ? { req } : {}
     const accessTokenOption = prismicAccessToken ? { accessToken: prismicAccessToken } : {}
     return {
