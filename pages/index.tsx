794e3c5cac7a45ef7a612b245a74fc33bbd7825b
nexthis <pawel.romanowski000@gmail.com>
2021-08-20 20:14:24 +0200

    change images

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import clsx from 'clsx'
 import style from '../styles/index.module.scss'
 import Layout from "../components/layout/base";
 import ContentInterface from '../public/languages/index/type'
-import profilePic from '../public/image/me.jpg'
+import profilePic from '../public/image/me.png'
 import {Client} from '../helpers/prismic'
 
 type props = {
@@ -21,9 +21,12 @@ const Home: FunctionComponent<props> = ({content}) => {
             <FullPage>
                 <FullPageItem>
                     <div className="h-full flex flex-col justify-around md:flex-row-reverse md:items-center">
-                        <div className='flex justify-center flex-1'>
-                            <div className={style.image}>
-                                <Image src={profilePic} className='rounded-full md:rounded-none' alt="Picture of the author"/>
+                        <div className='flex justify-center flex-1 h-full'>
+                            <div className="h-full w-2/3">
+                                <div className="aspect-w-4 aspect-h-5">
+                                    <Image src={profilePic} className='rounded-full md:rounded-none' layout="fill" objectFit="fill"
+                                           alt="Picture of the author"/>
+                                </div>
                             </div>
                         </div>
                         <div className="mb-9 flex-1">
