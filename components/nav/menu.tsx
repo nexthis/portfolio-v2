7cb9cc00fef398af1b40a182d17eab75c8ca1e68
nexthis <pawel.romanowski000@gmail.com>
2021-09-11 20:37:43 +0200

    fix menu

--- components/nav/menu.tsx
+++ components/nav/menu.tsx
@@ -1,36 +1,39 @@
 import styles from './style.module.scss'
-import {useState} from "react";
+import {FunctionComponent, useState} from "react";
 import {useSpring, animated} from "@react-spring/web";
-
 
 
 const Menu = () => {
     const [isOpen, toggle] = useState(false);
+    return (
+        <div>
+            <button type="button" aria-label="menu" className={styles.hamburger} onClick={() => toggle(!isOpen)}>
+                <Hamburger open={isOpen}/>
+            </button>
+        </div>
+    )
+}
+
+const Hamburger: FunctionComponent<{ open: boolean }> = ({open}) => {
 
     const first = useSpring({
-        transform: isOpen
-            ? "translate(0, 6px) rotate(-45deg)"
-            : "translate(0, 0) rotate(0deg)"
+        transform: open ? "translate(0px, 6px) rotate(-45deg)" : "translate(0px, 0px) rotate(0deg)",
     })
     const second = useSpring({
-        opacity: isOpen ? 0 : 1,
-        transform: isOpen
-            ? "translate(0, 10px) rotate(0deg)"
-            : "translate(0, 0) rotate(0deg)"
+        opacity: open ? 0 : 1,
+        transform: open ? "translate(10px, 0px) rotate(0deg)" : "translate(0px, 0px) rotate(0deg)",
+        config: {duration: 250}
     })
     const third = useSpring({
-        transform: isOpen
-            ? "translate(0, -12px) rotate(45deg)"
-            : "translate(0, 0) rotate(0deg)"
+        transform: open ? "translate(0, -12px) rotate(45deg)" : "translate(0px, 0px) rotate(0deg)",
     })
 
-
-    return(
-        <button type="button" aria-label="menu" className={styles.hamburger} onClick={() => toggle(!isOpen)}>
+    return (
+        <>
             <animated.div style={first} className={styles.hamburgerItem}/>
             <animated.div style={second} className={styles.hamburgerItem}/>
             <animated.div style={third} className={styles.hamburgerItem}/>
-        </button>
+        </>
     )
 }
 
