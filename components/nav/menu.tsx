7b1b77904e2a59328e3ad0ccee40d261ead6da9c
nexthis <pawel.romanowski000@gmail.com>
2021-09-11 20:49:59 +0200

    fix menu

--- components/nav/menu.tsx
+++ components/nav/menu.tsx
@@ -1,20 +1,24 @@
 import styles from './style.module.scss'
-import {FunctionComponent, useState} from "react";
+import {FunctionComponent, useEffect, useState} from "react";
 import {useSpring, animated} from "@react-spring/web";
+import {useHover} from "react-use-gesture";
+import clsx from "clsx";
 
 
 const Menu = () => {
     const [isOpen, toggle] = useState(false);
+    const [isHover, setHover] = useState(false);
+    const hovering = useHover((state) => setHover(state.hovering) );
     return (
         <div>
-            <button type="button" aria-label="menu" className={styles.hamburger} onClick={() => toggle(!isOpen)}>
-                <Hamburger open={isOpen}/>
+            <button {...hovering()} type="button" aria-label="menu" className={styles.hamburger} onClick={() => toggle(!isOpen)}>
+                <Hamburger hover={isHover} open={isOpen}/>
             </button>
         </div>
     )
 }
 
-const Hamburger: FunctionComponent<{ open: boolean }> = ({open}) => {
+const Hamburger: FunctionComponent<{ open: boolean, hover: boolean }> = ({open, hover}) => {
 
     const first = useSpring({
         transform: open ? "translate(0px, 6px) rotate(-45deg)" : "translate(0px, 0px) rotate(0deg)",
@@ -22,16 +26,23 @@ const Hamburger: FunctionComponent<{ open: boolean }> = ({open}) => {
     const second = useSpring({
         opacity: open ? 0 : 1,
         transform: open ? "translate(10px, 0px) rotate(0deg)" : "translate(0px, 0px) rotate(0deg)",
-        config: {duration: 250}
+        width: "70%",
+        config: {duration: 200}
     })
     const third = useSpring({
         transform: open ? "translate(0, -12px) rotate(45deg)" : "translate(0px, 0px) rotate(0deg)",
     })
 
+    useEffect(() => {
+        if(hover){
+            second.width.start('100%') ;
+        }
+    }, [hover])
+
     return (
         <>
             <animated.div style={first} className={styles.hamburgerItem}/>
-            <animated.div style={second} className={styles.hamburgerItem}/>
+            <animated.div style={second} className={clsx(styles.hamburgerItem, 'ml-auto')}/>
             <animated.div style={third} className={styles.hamburgerItem}/>
         </>
     )
