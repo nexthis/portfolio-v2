008140ec30fe4623d595ca7ce21617fd61f3b711
nexthis <pawel.romanowski000@gmail.com>
2021-09-28 22:57:47 +0200

    add support apple-touch-icon

--- components/head/head.tsx
+++ components/head/head.tsx
@@ -24,9 +24,23 @@ const HeadComponent: FunctionComponent<props> = (
             <meta name="description" content={seo.description}/>
 
             {/*PWA*/}
+            <link rel="manifest" href="/manifest.json"/>
+            <link rel="apple-touch-icon" sizes="57x57" href="/icons/pwa/icon-57x57.png"/>
+            <link rel="apple-touch-icon" sizes="60x60" href="/icons/pwa/icon-60x60.png"/>
+            <link rel="apple-touch-icon" sizes="72x72" href="/icons/pwa/icon-72x72.png"/>
+            <link rel="apple-touch-icon" sizes="76x76" href="/icons/pwa/icon-76x76.png"/>
+            <link rel="apple-touch-icon" sizes="114x114" href="/icons/pwa/icon-114x114.png"/>
+            <link rel="apple-touch-icon" sizes="120x120" href="/icons/pwa/icon-120x120.png"/>
+            <link rel="apple-touch-icon" sizes="144x144" href="/icons/pwa/icon-144x144.png"/>
+            <link rel="apple-touch-icon" sizes="152x152" href="/icons/pwa/icon-152x152.png"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/icons/pwa/icon-180x180.png"/>
+
+            <link rel="icon" type="image/png" sizes="192x192" href="/icons/pwa/favicon-192x192.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/icons/pwa/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="96x96" href="/icons/pwa/favicon-96x96.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/icons/pwa/favicon-16x16.png"/>
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
             <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-            <link rel="manifest" href="/manifest.json"/>
 
             {/*Phone color bar*/}
             <meta name="theme-color" content="#24262E"/>
@@ -34,6 +48,7 @@ const HeadComponent: FunctionComponent<props> = (
             {/*Control the behavior of search engine crawling and indexing*/}
             <meta name="robots" content="index,follow"/>
             <meta name="googlebot" content="index,follow"/>
+
 
             {/*Facebook Open Graph*/}
             <meta property="og:url" content={getAbsoluteURL(router.asPath)}/>
