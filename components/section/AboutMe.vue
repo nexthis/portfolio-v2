e767b546274bf5261fa85ff081349e1a7bcca91e
nexthis <pawel.romanowski000@gmail.com>
2023-11-11 21:59:49 +0100

    fix misspellings

--- components/section/AboutMe.vue
+++ components/section/AboutMe.vue
@@ -39,7 +39,7 @@ const localePath = useLocalePath()
       "quote": "If you are looking for a specialist to create a modern website, then you must see my portfolio!",
       "description": [
         "I am a programmer with 4 years of experience in creating web and desktop applications.",
-        "I specialize in technologies such as Vue.js, TypeScrip, Node Js, Directus, Laravel",
+        "I specialize in technologies such as Vue.js, TypeScript, Node Js, Directus, Laravel",
         "I had the opportunity to create and collaborate with many companies and institutions, which gave me the opportunity to work on both public and internal projects.",
         "My second passion, besides programming, is psychology, which helps me to better communicate with my clients and colleagues."
       ],
@@ -51,7 +51,7 @@ const localePath = useLocalePath()
       "quote": "Jeśli szukasz specjalisty do stworzenia nowoczesnej strony internetowej, to koniecznie musisz zobaczyć moje portfolio!",
       "description": [
         "Jestem programista z 4-letnim doświadczeniem w tworzeniu aplikacji webowych i desktopowych.",
-        "Specjalizuję się w technologiach takich jak Vue.js, TypeScrip, Node Js, Directus, Laravel",
+        "Specjalizuję się w technologiach takich jak Vue.js, TypeScript, Node Js, Directus, Laravel",
         "Miałem okazję tworzyć i współpracować z wieloma firmami i instytucjami, co dało mi możliwość pracy zarówno w różnych projektach publicznych, jak i wewnętrznych.",
         "Moją drugą pasją, obok programowania, jest psychologia, która pomaga mi lepiej komunikować się z moimi klientami i współpracownikami."
       ],
