ceecc8a431d9895962bc721730044559d2ae0fcd
nexthis <pawel.romanowski000@gmail.com>
2024-01-10 15:52:05 +0100

    fix mobile

--- components/SuperExtraCanvas.vue
+++ components/SuperExtraCanvas.vue
@@ -5,6 +5,13 @@ const { t } = useI18n({ useScope: 'local' })
 // const { width, height } = useWindowSize({ includeScrollbar: false })
 const { init } = useSuperExtraCanvas()
 
+function scroll() {
+  window.scrollTo({
+    behavior: 'smooth',
+    top: window.innerHeight
+  })
+}
+
 onMounted(async () => {
   await nextTick()
   if (canvasContainer.value) {
@@ -21,13 +28,21 @@ onMounted(async () => {
       <div ref="canvasContainer" class="canvas" />
     </ClientOnly>
 
-    <div class="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 pointer-events-none">
-      <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none lg:mb-6 md:text-5xl xl:text-6xl text-white whitespace-nowrap">
-        Paweł Romanowski
-      </h1>
-      <p class="font-light md:text-lg xl:text-xl text-gray-400">
-        {{ t('title') }}
-      </p>
+    <div class="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 ">
+      <div class="pointer-events-none use">
+        <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none lg:mb-6 md:text-5xl xl:text-6xl text-white whitespace-nowrap select-none	">
+          Paweł Romanowski
+        </h1>
+        <p class="font-light md:text-lg xl:text-xl text-gray-400 select-none	">
+          {{ t('title') }}
+        </p>
+      </div>
+
+      <div class="mt-5 text-center touch-only z-20">
+          <button @click="scroll" class="inline-block py-3 px-10 text-white font-bold bg-primary hover:bg-primary-600 rounded-full" data-cursor-hover>
+            {{ t('explore') }}
+          </button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,17 +56,27 @@ onMounted(async () => {
 }
 
 .canvas{
-overflow: hidden;
+  overflow: hidden;
+}
+
+.touch-only {
+  display: none;
+  @media (hover: none), (hover: on-demand), (-moz-touch-enabled: 1), (pointer:coarse) {
+    display: block;
+  }
 }
 </style>
 
 <i18n lang="json">
   {
     "en": {
-      "title": "Modern websites"
+      "title": "Modern websites",
+      "explore": "See more"
+
     },
     "pl": {
-      "title": "Nowoczesne strony internetowe"
+      "title": "Nowoczesne strony internetowe",
+      "explore": "Zobacz więcej"
     }
   }
   </i18n>
