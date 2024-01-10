31c6078a9b7dfc72a4723e59e21bc87f7a63d898
nexthis <pawel.romanowski000@gmail.com>
2024-01-10 16:11:41 +0100

    update sitemap

--- server/api/_sitemap-urls.ts
+++ server/api/_sitemap-urls.ts
@@ -4,11 +4,22 @@ import { serverQueryContent } from '#content/server'
 export default defineEventHandler(async (event) => {
   const data = await serverQueryContent(event).find()
 
-  return data.map((item) => {
+
+  const pl = data.map((item) => {
     return {
       loc: `portfolio/${item.slug}`,
       lastmod: new Date(item.datePublished),
       priority: 0.5
     }
   })
+
+  const en = data.map((item) => {
+    return {
+      loc: `en/portfolio/${item.slug}`,
+      lastmod: new Date(item.datePublished),
+      priority: 0.5
+    }
+  })
+
+  return [...en, ...pl]
 })
